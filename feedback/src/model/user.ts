ceccb8a169e0ecc94524cd1328db088a1d8feab2
sumit kumar <sumokhan76342@gmail.com>
2024-04-09 23:29:29 +0530

    added model

---
+++ feedback/src/model/user.ts
@@ -0,0 +1,66 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: [true, "Content is required"]
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "username is required"],
+        unique: true,
+        minlength: 3,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/.+\@.+\..+/, "please use a valid email address"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Password is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true,
+    },
+    messages: [MessageSchema]
+})
